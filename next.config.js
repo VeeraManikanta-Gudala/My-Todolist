7a07e22fa20802ebf88859d1b3f6b5b518eea416
Veera Manikanta <gveera_it211229@mgit.ac.in>
2023-11-28 18:46:40 +0530

    removed few bugs added some things

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
     output: "export"
 };
-=======
-  output: "export"
-}
->>>>>>> origin/main
 
 module.exports = nextConfig
